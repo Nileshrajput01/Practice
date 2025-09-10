64e91f36766c91d1b124811b5c17ef1a895665ee
Nileshrajput01 <nileshrajput121202@gmail.com>
2025-09-10 16:26:38 +0530

    asian y new guy

---
+++ src/mypractice/java4.java
@@ -0,0 +1,11 @@
+package mypractice;
+
+public class java4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.println("gjhgbhg wefkjdfh wfhdwgf as jhbsacb ************"
+		 		+ " fdhfdgfdsfdhbfdhgbdfv ");
+	}
+
+}
